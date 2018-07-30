5b2c2ab63cd1bc98e6acedb3d7f7014b10005799
Leanne Egan <lmegan92@gmail.com>
2018-07-30 12:47:01 +0800

    Created models, routes and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+content = open(url).read
+content = JSON.parse(content)
+
+content["drinks"].each do |ingredient|
+  ingre = Ingredient.new(name: ingredient["strIngredient1"])
+  ingre.save
+end
